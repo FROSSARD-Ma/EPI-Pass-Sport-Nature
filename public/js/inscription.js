d8151ecb2f059018e2693b1dd8d5ca064c8d9283
Marie.FROSSARD <marie@spotsweb.fr>
2020-04-12 08:01:32 +0200

    Formulaire inscription par étapes

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -1,33 +1,87 @@
 class Inscription {
 
-  constructor(formElt, statutGp, groupeElt, userElt, nameLabel, btnForm) {
+    constructor(formElt, statut, groupeElt, gpH3, gpName, gpMail, userElt, nameLabel, userName, userFirstname, userMail, userPass, inscBtn, nextBtn, prevBtn) {
 
-    // DOM selectors
-    this.formulaire   = formElt;
-    this.statutGp     = statutGp;
-    this.groupeElt    = groupeElt;
-    this.userElt      = userElt;
-    this.nameLabel    = nameLabel;
-  }
+        // DOM selectors
+        this.formulaire     = formElt;
+        this.gpStatut       = statut;
 
-  editForm()
-  {
-    if (this.statutGp =='Particulier')
-    {
-        this.groupeElt.style.display = 'none';
-        this.userElt.style.display = 'block';
+        this.groupeElt      = groupeElt;
+        this.gpH3           = gpH3;
+        this.h3             = this.gpStatut.value;
+        this.gpName         = gpName;
+        this.gpMail         = gpMail;
+
+        this.userElt        = userElt;
+        this.nameLabel      = nameLabel;
+        this.userName       = userName;
+        this.userFirstname  = userFirstname;
+        this.userMail       = userMail;
+        this.userPass       = userPass;
+
+        this.inscBtn         = inscBtn;
+        this.nextBtn        = nextBtn;
+        this.prevBtn        = prevBtn;
     }
-    else if (this.statutGp !='')
+
+    
+    refreshForm()
     {
-        this.nameLabel.innerHTML += " du responsable";
+
+        if (this.gpStatut =='Particulier')
+        {
+            this.nameLabel.innerHTML = "Nom";
+            this.userSection();
+        }
+        else if (this.gpStatut !='')
+        {
+            this.nameLabel.innerHTML = "Nom du responsable";
+            this.groupeSection();
+        }
+        else
+        {
+             this.statutSection();
+        }
+    }
+
+    statutSection()
+    {
+        this.prevBtn.style.display = 'none';
+        this.nextBtn.style.display = 'none';
+        this.inscBtn.style.display = 'none';
+    }
+
+    groupeSection()
+    {
+        
         this.groupeElt.style.display = 'block';
-        this.userElt.style.display = 'block';
-    }
-    else
-    {
-        this.groupeElt.style.display = 'none';
+        this.gpH3.innerHTML = this.h3;
         this.userElt.style.display = 'none';
+
+        /*inscBtn*/
+        this.prevBtn.style.display = 'none';
+        this.nextBtn.style.display = 'block';
+        this.inscBtn.style.display = 'none';
+
+        this.nextBtn.addEventListener('click', () =>
+        {
+            this.prevBtn.style.display = 'block';
+            this.userSection();
+        });
     }
 
-  }
+    userSection()
+    {
+        this.groupeElt.style.display = 'none';
+        this.userElt.style.display = 'block';
+        /*inscBtn*/
+        this.nextBtn.style.display = 'none';
+        
+        this.inscBtn.style.display = 'block';
+
+        this.prevBtn.addEventListener('click', () =>
+        {
+            this.groupeSection();
+        });
+    }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,66 @@
 
-// INSCRIPTION -------------------------------------------------------
-
-let inscForm = document.getElementById('inscForm');
-let gpStatut = document.getElementById('groupeStatut');
-
-/* == GESTIONS EVENEMENTS ================================ */
-
-gpStatut.addEventListener('change', () =>
-{	
-	let statut = gpStatut.value;
-	let groupeElt = inscForm.elements.groupe;
-	let userElt = inscForm.elements.user;
-	let nameLabel = inscForm.elements.labelUserName;
-	let btnInsc = inscForm.elements.button;
-
-	nxInscription = new Inscription (inscForm, statut, groupeElt, userElt, nameLabel, btnInsc);
-	nxInscription.editForm();
-});
 
 
-const form = document.querySelector('form');
-$('form').parsley();
-let button = form.querySelector('button[type=submit]');
-let buttonText = button.textContent;
-
-
-// CHANGE MAIL -------------------------------------------------------
 let mailForm = document.getElementById('mailForm');
 
 let message = document.getElementById('message');
 
+
+// FORMULAIRE --------------------------------------------
+const form = document.querySelector('form');
+let nextBtn = document.getElementById('next-btn');
+let prevBtn = document.getElementById('previous-btn');
+let button = form.querySelector('button[type=submit]');
+
+$('form').parsley();
+
+// INSCRIPTION -------------------------------------------
+let inscForm = document.getElementById('form-inscription');
+let gpStatut = document.getElementById('groupeStatut');
+let statut = gpStatut.value;
+let inscBtn = inscForm.querySelector('button[type=submit]');
+inscBtn.style.display = 'none';
+
+/* groupe */
+const groupeElt = document.getElementById('groupe');
+groupeElt.style.display = 'none';
+let gpH3= groupeElt.getElementsByTagName("h3");
+let gpName = document.getElementById('groupeName');
+let gpMail = document.getElementById('groupeMail');
+/* user resp */
+const userElt = document.getElementById('user');
+userElt.style.display = 'none';
+let nameLabel = document.getElementById('labelUserName');
+let userName = document.getElementById('userName');
+let userFirstname = document.getElementById('userFirstname');
+let userMail = document.getElementById('userMail');
+let userPass = document.getElementById('userPass');
+
+
 /* == GESTIONS EVENEMENTS ================================ */
+gpStatut.addEventListener('change', (event) => {	
+	gpStatut = event.target.value;
+	let nxInscription = new Inscription(inscForm, gpStatut, groupeElt, gpH3, gpName, gpMail, userElt, nameLabel, userName, userFirstname, userMail, userPass, inscBtn, nextBtn, prevBtn);
+	nxInscription.refreshForm();
+});
 
-mailForm.addEventListener('submit', function (e) {
 
-  	button.disabled = true;
-  	button.textContent = 'Chargement...';
 
- 	e.preventDefault()
 
- 	const formData = new FormData(this);
+// mailForm.addEventListener('submit', function (e) {
+//		let buttonText = button.textContent;
+//   	button.disabled = true;
+//   	button.textContent = 'Chargement...';
 
- 	nxForm = new Formulaire (mailForm, formData);
-	nxForm.verifForm();
+//  	e.preventDefault()
 
-	// Dans tous les cas on permet la soumission du formulaire à nouveau
-	button.disabled = false;
-	button.textContent = buttonText;
+//  	const formData = new FormData(this);
 
-})
+//  	nxForm = new Formulaire (mailForm, formData);
+// 	nxForm.verifForm();
+
+// 	button.disabled = false;
+// 	button.textContent = buttonText;
+
+// })
 
