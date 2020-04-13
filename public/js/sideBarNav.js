bde6e515324b377e002931ae005891eb765360f7
Marie.FROSSARD <marie@spotsweb.fr>
2020-04-13 11:56:46 +0200

    Ajout Menu NAV dans slidebar

---
+++ public/js/sideBarNav.js
@@ -0,0 +1,16 @@
+// SIDEBAR NAV --------------------------------------------
+
+let dashboardBtn = document.getElementById('dashboard-btn');
+
+let addUserBtn = document.getElementById('addUser-btn');
+addUserBtn.style.display = 'none';
+
+let equipBtn = document.getElementById('equip-btn');
+
+let equipBtnElt = document.getElementById('equip-btn-elt');
+equipBtnElt.style.display = 'none';
+
+let calendarBtn = document.getElementById('calendar-btn');
+let historyBtn = document.getElementById('history-btn');
+
+
