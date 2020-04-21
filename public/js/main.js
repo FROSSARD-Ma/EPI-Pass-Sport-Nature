e0c0980979f3d56312a4f26bfa05f7e13be415b0
Marie.FROSSARD <marie@spotsweb.fr>
2020-04-21 07:55:14 +0200

    installation datatables
    - utilisation composer

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,10 @@ let message = document.getElementById('message');
 
 // FORMULAIRE --------------------------------------------
 const form = document.querySelector('form');
-
 $('form').parsley();
 
 
-// var submitBtn = document.querySelectorAll('button[type=submit]');
-// for (var i = 0; i < submitBtn.length; i++) {
-//   submitBtn[i].onclick = validSubmit;
-// }
-// function validSubmit(e) {
-// console.log('coucou');
-//   e.preventDefault()
-//  	const formData = new FormData(submitBtn[i]);
-//  	const nxForm = new Formulaire (submitBtn[i], formData);
-// 	nxForm.verifForm();
-// }
+// TABLEAU --------------------------------------------
+$(document).ready(function() {
+    $('#tableEquipt').DataTable();
+} );
