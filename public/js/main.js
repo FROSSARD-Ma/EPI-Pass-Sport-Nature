c3401c680be4e0c5fb2168cf20ac9529360f6a3b
Marie.FROSSARD <marie@spotsweb.fr>
2020-04-23 18:34:30 +0200

    Ajout controle taille fichier image avec parsley JS

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,22 @@ const form = document.querySelector('form');
 $('form').parsley();
 
 
+window.Parsley.addValidator('maxFileSize', {
+  validateString: function(_value, maxSize, parsleyInstance) {
+    if (!window.FormData) {
+      alert('You are making all developpers in the world cringe. Upgrade your browser!');
+      return true;
+    }
+    var files = parsleyInstance.$element[0].files;
+    return files.length != 1  || files[0].size <= maxSize * 1024;
+  },
+  requirementType: 'integer',
+  messages: {
+    fr: 'Ce fichier est plus grand que %s Kb.'
+  }
+});
+
+
 // TABLEAU --------------------------------------------
 $(document).ready(function() {
     $('#tableEquipt').DataTable();
