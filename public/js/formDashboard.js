938f5e0440a5ee1b90a6c3c1023fa2af1f28ec35
Marie.FROSSARD <marie@spotsweb.fr>
2020-04-30 11:00:41 +0200

    Dashboard  - UserForm en JS
    MAJ fichier formDashboard
    - Boucle for sur les formulaire pour prendre en compte chaque ligne d'utilisateur
    
    - Supression controle en JS du formulaire ajout User pour rafraichissement page et affochage direct.

--- public/js/formDashboard.js
+++ public/js/formDashboard.js
@@ -1,33 +1,15 @@
 
 /* == Update User STATUT  ================================ */
-	let upUserForm = document.getElementById('upUserForm');
-	let upUserBtn = upUserForm.querySelector('button[type=submit]');
+
+const userForm  = document.getElementById('users');
+form = userForm.querySelectorAll('form');
+
+for (let i=0, nbForm=form.length; i<nbForm; i++) {
 	
-	upUserForm.addEventListener('submit', function (e) {
-	 	e.preventDefault()
-	 	let formData = new FormData(this);
-	 	let nxForm = new Formulaire (this, formData);
-		nxForm.verifForm();
-	})
-
-/* == Delete USER  ======================================= */
-	let delUserForm = document.getElementById('delUserForm');
-	let delUserBtn = delUserForm.querySelector('button[type=submit]');
-
-	delUserForm.addEventListener('submit', function (e) {
-	 	e.preventDefault()
-	 	let formData = new FormData(this);
-	 	let nxForm = new Formulaire (this, formData);
-		nxForm.verifForm();
-	})
-
-/* == Ajout USER  ================================ */
-	let addUserForm = document.getElementById('addUserForm');
-	let addUserBtn = addUserForm.querySelector('button[type=submit]');
-
-	addUserForm.addEventListener('submit', function (e) {
-	 	e.preventDefault()
-	 	let formData = new FormData(this);
-	 	let nxForm = new Formulaire (this, formData);
-		nxForm.verifForm();
-	})
+    form[i].addEventListener('submit', function (e) {
+    	e.preventDefault();
+    	let formData = new FormData(this);
+	 		let nxForm = new Formulaire (this, formData);
+	 	nxForm.verifForm();
+    });
+}
